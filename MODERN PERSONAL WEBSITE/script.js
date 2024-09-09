a1b52699e6d95c55616da6ca51648600b6d97da7
justindy81 <149914709+justindy81@users.noreply.github.com>
2024-09-09 17:15:11 -0500

    Delete MODERN PERSONAL WEBSITE directory

--- MODERN PERSONAL WEBSITE/script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navlinks = document.querySelector('.nav-links');
-
-menuIcon.onclick = () => {
-    navlinks.classList.toggle('active');
-}
