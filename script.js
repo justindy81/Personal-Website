2ea771aae2ae0e1c364970281b1be10ceffa6225
justindy81 <149914709+justindy81@users.noreply.github.com>
2024-09-10 17:58:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const navlinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navlinks.classList.toggle('active');
 }
+
+document.getElementById('live-demo-btn').addEventListener('click', function() {
+    window.open('demo1.mp4', '_blank');
+});
+
+document.getElementById('github-repo-btn').addEventListener('click', function() {
+    window.open('https://github.com/justindy81/marketplace-project-1', '_blank');
+});
+
+document.getElementById('live-demo-btn1').addEventListener('click', function() {
+    window.open('demo2.mp4', '_blank');
+});
+
+document.getElementById('github-repo-btn1').addEventListener('click', function() {
+    window.open('https://github.com/justindy81/Responsive-Personal-RGB-Website', '_blank');
+});
+
+document.getElementById('github-repo-btn2').addEventListener('click', function() {
+    window.open('https://github.com/justindy81/URL-to-Video-AI-Project', '_blank');
+});
